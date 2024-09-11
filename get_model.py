e71c102c5f4254f76f16bbbed6fafba44118ee8c
Omar Elgendy <omaratef3221@gmail.com>
2024-09-11 19:54:15 +0400

    device map balanced

--- get_model.py
+++ get_model.py
@@ -21,7 +21,7 @@ def get_model(model_id):
 
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     model = AutoModelForCausalLM.from_pretrained(model_path, 
-                                                device_map="auto", 
+                                                device_map="balanced", 
                                                 trust_remote_code=True, 
                                                 offload_folder="offload", 
                                                 # torch_dtype = torch.bfloat16
