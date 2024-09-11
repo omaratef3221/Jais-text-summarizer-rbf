21d09ea168591c747807b077f825ed2fc6d98e8e
Omar Elgendy <omaratef3221@gmail.com>
2024-09-11 20:48:40 +0400

    epoch not epochs

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def main(args):
     save_steps=1,
     learning_rate=1e-4,
     push_to_hub = True,
-    logging_strategy='epochs'
+    logging_strategy='epoch'
     # bf16 = True,
     # hub_model_id = f"basic-jais-13b-arabic-text-summarizer",
     # push_to_hub_model_id = f"basic-jais-13b-arabic-text-summarizer",
